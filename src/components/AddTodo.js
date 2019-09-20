3d868e70e4c16cabf8e29f162fe2964dafb0d1b9
saburahmed <saburahmed@gmail.com>
2019-09-20 18:44:46 +0100

    added jsonplaceholder

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
 
 export class AddTodo extends Component {
     state = {
@@ -22,5 +23,12 @@ export class AddTodo extends Component {
     );
   }
 }
+
+// PropTypes
+AddTodo.propTypes = {
+  addTodo: PropTypes.func.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired,
+};
 
 export default AddTodo;
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This TodoList app is part of a React Crash course video I watched</p>
+        </React.Fragment>
+    )
+}
+
+export default About;
